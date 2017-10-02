756a8e97dc5a9bd938dc53ec511446d0bbe0829b
Maryadi <maryadi@astrnt.co>
2017-10-02 17:18:09 +0700

    add long click and normal click

--- app/src/main/java/com/studio/poipo/longpressedbuttonpopupmenu/MainActivity.java
+++ app/src/main/java/com/studio/poipo/longpressedbuttonpopupmenu/MainActivity.java
@@ -2,12 +2,32 @@ package com.studio.poipo.longpressedbuttonpopupmenu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button testButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        testButton = (Button) findViewById(R.id.button);
+        testButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Normal Click", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        testButton.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                Toast.makeText(getApplicationContext(),"Long Click", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
     }
 }
